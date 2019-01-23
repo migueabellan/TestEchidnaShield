8a782659e3239e83bc8417286199f9172bab24a4
Miguel Ángel Abellán <migueabellan@users.noreply.github.com>
2019-01-23 09:44:03 +0100

    Update TestEchidnaShield.ino

--- TestEchidnaShield.ino
+++ TestEchidnaShield.ino
@@ -1,11 +1,11 @@
-const int MAX_SENS = 19;
+const int MAX_SENS = 27;
 String result[MAX_SENS];
 int index = 0;
 
 void setup() {
   Serial.begin(9600);
 
-  printInit();
+  printInitSens();
 
   // Buttons
   testButton(2, "SR D2");
@@ -33,10 +33,22 @@ void setup() {
   testJoystick(1, 0, "Joystick A1 (Abajo)");
 
   // Acelerometro
-  testAceler(2, 0, "Acelerómetro A2X (Izquierda)");
-  testAceler(2, 1, "Acelerómetro A2X (Derecha)");
-  testAceler(3, 1, "Acelerómetro A3Y (Adelante)");
-  testAceler(3, 0, "Acelerómetro A3Y (Atrás)");
+  testAccel(2, 0, "Acel A2X (Izquierda)");
+  testAccel(2, 1, "Acel A2X (Derecha)");
+  testAccel(3, 1, "Acel A3Y (Adelante)");
+  testAccel(3, 0, "Acel A3Y (Atrás)");
+
+  printInitMkmk();
+  
+  // Mkmk
+  testMkmk(0, 0, "Mkmk A0");
+  testMkmk(1, 0, "Mkmk A1");
+  testMkmk(2, 0, "Mkmk A2");
+  testMkmk(3, 0, "Mkmk A3");
+  testMkmk(4, 0, "Mkmk A4");
+  testMkmk(5, 0, "Mkmk A5");
+  testMkmk(2, 1, "Mkmk D2");
+  testMkmk(3, 1, "Mkmk D3");
 
   printResult();
 }
@@ -46,12 +58,13 @@ void loop() {
 
 
 void testButton(int pin, String text) {
+  pinMode(pin, INPUT);
+  
   unsigned long time = millis();
   int timeout = 5000;
   bool isTimeout = false;
   
   Serial.println("Pulsa el botón " + text);
-
 
   while(digitalRead(pin) == LOW) { 
     if(millis() - time > timeout) {
@@ -65,6 +78,8 @@ void testButton(int pin, String text) {
 }
 
 void testLed(int pin, String text) {
+  pinMode(pin, OUTPUT);
+  
   Serial.println("¿Está encendido el " + text + "?");
 
   analogWrite(pin, 255);
@@ -73,6 +88,8 @@ void testLed(int pin, String text) {
 }
 
 void testBuzzer(int pin, String text) {
+  pinMode(pin, OUTPUT);
+  
   Serial.println("¿Ha sonado el " + text + "?");
 
   for(int i=1; i<10; i++) {
@@ -88,7 +105,6 @@ void testLdr(int pin, String text) {
   bool isTimeout = false;
   
   Serial.println("Tapa el " + text);
-
 
   while(analogRead(pin) > 200) { 
     if(millis() - time > timeout) {
@@ -110,7 +126,7 @@ void testJoystick(int pin, int op, String text) {
   
   switch(op) {
     case 0:
-      while(analogRead(pin) > 240) { 
+      while(analogRead(pin) > 256) { 
         if(millis() - time > timeout) {
           saveResult(false, text);
           isTimeout = true;
@@ -119,7 +135,7 @@ void testJoystick(int pin, int op, String text) {
       }
       break;
     case 1:
-      while(analogRead(pin) < 400) {
+      while(analogRead(pin) < 768) {
         if(millis() - time > timeout) {
           saveResult(false, text);
           isTimeout = true;
@@ -133,13 +149,11 @@ void testJoystick(int pin, int op, String text) {
 }
 
 
-void testAceler(int pin, int op, String text) {
+void testAccel(int pin, int op, String text) {
   unsigned long time = millis();
   int timeout = 5000;
   bool isTimeout = false;
 
-  Serial.println("Apoya la placa sobre la mesa ... ");
-  delay(1000);
   Serial.println("Mueve " + text);
 
   switch(op) {
@@ -166,9 +180,44 @@ void testAceler(int pin, int op, String text) {
   if(!isTimeout) saveResult(true, text);
 }
 
+void testMkmk(int pin, int op, String text) {
+  unsigned long time = millis();
+  int timeout = 5000;
+  bool isTimeout = false;
+  
+  Serial.println("Toca el " + text);
+
+  switch(op) {
+    case 0:
+      while(analogRead(pin) < 500) { 
+        if(millis() - time > timeout) {
+          saveResult(false, text);
+          isTimeout = true;
+          break;
+        }  
+      }
+      break;
+    case 1:
+      pinMode(pin, INPUT);
+      while(digitalRead(pin) == LOW) {
+        if(millis() - time > timeout) {
+          saveResult(false, text);
+          isTimeout = true;
+          break;
+        }
+      }
+      break;
+  }
+  
+  if(!isTimeout) saveResult(true, text);
+}
+
 
 bool isCorrect() {
-  const int SR = 2, SL = 3;
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  
+  int SR = 2, SL = 3;
   bool ok, ko;
 
   Serial.println("- Pulsa SR si es correcto");
@@ -187,31 +236,35 @@ bool isCorrect() {
 
 
 void saveResult(bool op, String text) {
-  String aux = op ? "✓" : "✗";
-  aux+= " -> ";
-  aux+= text;
-  result[index++] = aux;
+  String status = op ? "✓" : "✗";
+  result[index++] = status + " -> " + text;
 
-  printSpace();
-  delay(1000);
+  Serial.println(" > " + status);
+  Serial.flush();
+  delay(500);
 }
 
-void printInit() {
-  Serial.println("---------------------------------------------------");
-  Serial.println("El test de EchidnaShield empezará en 5 segundos ...");
-  Serial.println("---------------------------------------------------");
+void printInitSens() {
+  Serial.println("---------------------------------");
+  Serial.println("Test de EchidnaShield (sensores):");
+  Serial.println(" > Fija Select en la Sens ...");
+  Serial.println("---------------------------------");
   delay(5000);
 }
 
-void printSpace() {
-  Serial.println("\n");
+void printInitMkmk() {
+  Serial.println("-----------------------------");
+  Serial.println("Test de EchidnaShield (MKMK):");
+  Serial.println(" > Fija Select en MKMK ...");
+  Serial.println("-----------------------------");
+  delay(5000);
 }
 
 void printResult() {
-  Serial.println("----------------------------------------------");
-  Serial.println("El resultado del test ha sido el siguiente ...");
+  Serial.println("-------------------");
+  Serial.println("Resultado del Test:");
   for(int i=0; i<MAX_SENS; i++) {
     Serial.println(result[i]);
   }
-  Serial.println("----------------------------------------------");
+  Serial.println("-------------------");
 }
